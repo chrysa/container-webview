83845559857df0f2202eb47bba93ed0ee1969694
chrysa <chrysa@users.noreply.github.com>
2026-05-24 15:53:36 +0200

    chore: chore/format-and-docs-update (#89)
    chore/format-and-docs-update
    
    ---------
    
    Co-authored-by: chrysa <greau.anthony+chrysa@gmail.com>

--- code/src/components/layouts/Layout.tsx
+++ code/src/components/layouts/Layout.tsx
@@ -1,15 +1,13 @@
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import GlobalLoader from "@/components/loaders/GlobalLoader";
-import { BackendConnectionBanner } from "@/components/ui/BackendConnectionBanner";
-import styles from "./Layout.module.scss";
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import GlobalLoader from '@/components/loaders/GlobalLoader';
+import styles from './Layout.module.scss';
 
 export default function Layout() {
   return (
     <div className={styles.layout}>
-      <BackendConnectionBanner />
       <Header />
       <div className={styles.body}>
         <Sidebar />
